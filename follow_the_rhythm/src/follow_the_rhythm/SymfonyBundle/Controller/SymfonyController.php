72dad927dce8c5c007fa5a1fc6f40a4e19d93f4a
Belderrain Jeremy <j.belderrain@iutbayonne.univ-pau.fr>
2018-03-10 16:36:24 +0000

    Modification/Suppression actualité ajouté

--- follow_the_rhythm/src/follow_the_rhythm/SymfonyBundle/Controller/SymfonyController.php
+++ follow_the_rhythm/src/follow_the_rhythm/SymfonyBundle/Controller/SymfonyController.php
@@ -17,9 +17,6 @@ use follow_the_rhythm\SymfonyBundle\Form\ConcertType;
 
 class SymfonyController extends Controller
 {
-  
-    
-    
     public function indexAction($page = 1, $sens = 1)
     {
       //-------------------------------AFFICHER ACTUALITE/ARTISTE/CONCERT---------------------------
@@ -115,20 +112,12 @@ class SymfonyController extends Controller
         
       // Envoi du formulaire vers la vue
       //return $this->render('follow_the_rhythmSymfonyBundle:Symfony:soumettreActualite.html.twig',array('formulaireActualite'=>$formulaireActualite->createView()));
-      return $this->redirect($this->generateUrl('follow_the_rhythm_accueil',array('page'=>1)));
+      return $this->redirect($this->generateUrl('follow_the_rhythm_accueil',array('page'=>1,'sens'=>1)));
     
       }
       return $this->render('follow_the_rhythmSymfonyBundle:Symfony:soumettreActualite.html.twig',
       array('formulaireActualite' => $formulaireActualite->createView()));
     }
-    
-    
-    
-    
-    
-    
-    
-    
     
     
     
@@ -203,7 +192,7 @@ class SymfonyController extends Controller
             'formulaireConcert'=>$formulaireConcert->createView()));
    
     }
-    //heyo
+    
     public function contactsAction(){
             return $this->render('follow_the_rhythmSymfonyBundle:Symfony:contacts.html.twig');
     }
@@ -290,121 +279,52 @@ class SymfonyController extends Controller
       return $this->render('follow_the_rhythmSymfonyBundle:Symfony:news.json.twig',
       array('tabActualites'=>$tabActualites));
     }
-    //--------------------------------------------------------METHODES TEST--------------------------------------------------------
     
-    /*public function ajouterAction()
-  {
-    // Création de l'entité
-    $actualite = new Actualite();
-    $actualite->setTitre('Mon dernier weekend');
-    $actualite->setDescription("C'était vraiment super et on s'est bien amusé.");
-    $actualite->setNbCoupDeCoeurs(1);
-    $actualite->setDate(2018/12/01);
-    // On peut ne pas définir ni la date ni la publication,
-    // car ces attributs sont définis automatiquement dans le constructeur
-
-    // On récupère l'EntityManager
-    $em = $this->getDoctrine()->getManager();
-
-    // Étape 1 : On « persiste » l'entité
-    $em->persist($actualite);
-
-    // Étape 2 : On « flush » tout ce qui a été persisté avant
-    $em->flush();
-
-    // Reste de la méthode qu'on avait déjà écrit
-    if ($this->getRequest()->getMethod() == 'POST') {
-      $this->get('session')->getFlashBag()->add('info', 'Actualite bien enregistré');
-      return $this->redirect( $this->generateUrl('sdzblog_voir', array('id' => $actualite->getId())) );
-    }
-
-    return $this->render('follow_the_rhythmSymfonyBundle:Symfony:index.html.twig');
-  }*/
-    
-    /*public function actualiteAction($titre,$description,$nbCoupDeCoeurs,$dateActualite,$unMod)
-    {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to your action: index(EntityManagerInterface $em)
-        $em = $this->getDoctrine()->getManager();
-
-        $actualite = new Actualite();
-        $actualite->setTitre($titre);
-        $actualite->setDescription($description);
-        $actualite->setNbCoupDeCoeurs($nbCoupDeCoeurs);
-        $actualite->setDateActualite($dateActualite);
-        $actualite->setModerateur($unMod);
-                
-       # $birthday = "11-11-2018";
-      #  $dateT = new \DateTime($birthday);
-        
-      #  $artiste->setDateActualite($dateT);
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $em->persist($actualite);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $em->flush();
-
-        return $this->render('follow_the_rhythmSymfonyBundle:Symfony:index.html.twig');
-        #return new Response('Saved new product with id '.$actualite->getId());
-    }*/
-    
-    /*public function soumettreActualiteAction(Request $requeteUtilisateur)
-    {
-      //création objet Actualite "vide"
-      $actualite = new Actualite();
+    public function modifierSupprimerActualiteAction(){
+      //--------------------------AFFICHER CONCERT----------------------------
+      //on récupère le gestionnaire d'entité
+      $gestionnaireEntite = $this->getDoctrine()->getManager();
       
-      //Création formulaire permettant de saisir une actualité
-      $formulaireActualite = $this->createFormBuilder($actualite)
-        ->add('titre','text')
-        ->add('artiste_id','text', array('required'=>false,'label'=>'Artiste Concerné'))
-        ->add('concert_id','text', array('required'=>false,'label'=>'Concert Concerné'))
-        ->add('description','textarea')
-        ->getForm();
+      //on récupère le repository des entités
+      $repositoryActualite = $gestionnaireEntite->getRepository('follow_the_rhythmSymfonyBundle:Actualite');
+      
+      //On récupère tous les concert de la BD ainsi que les Artistes liés
+      $tabActualites = $repositoryActualite->findAll();
+      
+      //On envoie les données à la vue correspondante
+      return $this->render('follow_the_rhythmSymfonyBundle:Symfony:modifierSupprimerActualite.html.twig', array('tabActualites'=>$tabActualites));
+    }
+    
+    public function modifierActualiteAction(Request $requeteUtilisateur, $id){
+       //on récupère le gestionnaire d'entité
+      $gestionnaireEntite = $this->getDoctrine()->getManager();
+      
+      //on récupère le repository des entités
+      $repositoryActualite = $gestionnaireEntite->getRepository('follow_the_rhythmSymfonyBundle:Actualite');
+      
+      $actualite = $repositoryActualite->find($id);
+      
+      $formulaireActualite = $this->createForm(new ActualiteType, $actualite); //création du formulaire
         
       //Récupération des données dans $actualite dès que le formulaire est soumis
       $formulaireActualite->handleRequest($requeteUtilisateur);
       
-      if($formulaireActualite->isSubmitted()) //Le formulaire a été soumis
+      if($formulaireActualite->isValid()) //Le formulaire a été soumis
       {
         //On enregistre l'objet $actualite dans la BD
-        $gestionnaireEntite = $this->getDoctrine()->getManager();
-        $actualite->setDateActualite(date('Y-m-d H:i:s'));
+        $actualite->setDateActualite(new \Datetime());
+        $actualite->setNbCoupDeCoeurs(0);
+        //On met à l'actualité le seul modérateur éxistant
+        //$actualite->setModerateur(xxx);   //IMPORTANT!
         $gestionnaireEntite->persist($actualite);
         $gestionnaireEntite->flush();
-      }
+        
       // Envoi du formulaire vers la vue
-      return $this->render('follow_the_rhythmSymfonyBundle:Symfony:index.html.twig', 
-                    array('formulaireActualite' => $formulaireActualite->createView()));
-    }*/
-}
-
-
-
-    /*
-    public function artisteAction()
-    {
-        // you can fetch the EntityManager via $this->getDoctrine()
-        // or you can add an argument to your action: index(EntityManagerInterface $em)
-        $em = $this->getDoctrine()->getManager();
-
-        $artiste = new Artiste();
-        $artiste->setNomComplet("Machin");
-        $artiste->setNomDeScene("Truc");
-        $artiste->setNbFollower(6);
-        
-       # $birthday = "11-11-2018";
-      #  $dateT = new \DateTime($birthday);
-        
-      #  $artiste->setDateActualite($dateT);
-
-        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-        $em->persist($artiste);
-
-        // actually executes the queries (i.e. the INSERT query)
-        $em->flush();
-
-        return $this->render('follow_the_rhythmSymfonyBundle:Symfony:index.html.twig');
-        #return new Response('Saved new product with id '.$artiste->getId());
+      //return $this->render('follow_the_rhythmSymfonyBundle:Symfony:soumettreActualite.html.twig',array('formulaireActualite'=>$formulaireActualite->createView()));
+      return $this->redirect($this->generateUrl('follow_the_rhythm_accueil',array('page'=>1,'sens'=>1)));
+    
+      }
+      return $this->render('follow_the_rhythmSymfonyBundle:Symfony:modifierActualite.html.twig',
+      array('formulaireActualite' => $formulaireActualite->createView()));
     }
-*/
+}
